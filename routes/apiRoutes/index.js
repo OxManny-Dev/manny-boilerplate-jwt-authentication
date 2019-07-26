e36d4184b25d9237d77d1b5ff37dd8e799e90135
emmanueljucaban <emmanueljucaban@gmail.com>
2019-07-26 14:10:06 -0700

    Configure signup, signin, and middlewares

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,18 @@
 const router      = require('express').Router();
 const authRoutes  = require('./authRoutes');
 
+const passportService = require('./../../services/passport');
+
+const authMiddleware = require('./../../middlewares/authMiddlewares');
 // / api prepended to these routes
+
+router.route('/test')
+  .get(authMiddleware.requireAuth, (req, res) => {
+    res.send(req.user);
+  });
+
 router.use('/auth', authRoutes);
+
+
 
 module.exports = router;

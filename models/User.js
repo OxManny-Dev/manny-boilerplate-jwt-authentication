087424dcecbe91854d03aea3abadd2d3509a21db
emmanueljucaban <emmanueljucaban@gmail.com>
2019-07-26 11:04:28 -0700

    add encryption to user email password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -22,6 +23,20 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true
+  }
+});
+
+UserSchema.pre('save', async function(next){
+  const user = this;
+  try {
+    const salt = await bcrypt.genSalt();
+    console.log('salt', salt);
+    const hash = await bcrypt.hash(user.password, salt);
+    console.log('hash', hash);
+    user.password = hash;
+    next();
+  } catch(e) {
+    return next(e);
   }
 });
 

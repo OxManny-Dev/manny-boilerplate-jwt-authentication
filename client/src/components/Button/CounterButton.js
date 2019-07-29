ad84ac2d8aed580557a5a925d6af9a673174b2d3
emmanueljucaban <emmanueljucaban@gmail.com>
2019-07-29 11:14:32 -0700

    Create counter container

---
+++ client/src/components/Button/CounterButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Button = props => {
+  return <button onClick={props.handleClick}>{props.children}</button>
+};
+
+export default Button;
---
+++ client/src/containers/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Button from './../components/Button/CounterButton';
+
+class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
+  handleIncrement = () => {
+    this.setState({ counter: this.state.counter + 1});
+  }
+
+  handleDecrement = () => {
+    this.setState({ counter: this.state.counter - 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>Counter: {this.state.counter}</p>
+        <Button handleClick={this.handleIncrement}>Increment</Button>
+        <Button handleClick={this.handleDecrement}>Decrement</Button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
